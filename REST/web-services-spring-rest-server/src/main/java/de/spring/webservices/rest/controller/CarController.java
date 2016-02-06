ed7f9084496092e1d3da70879cc14691b9efb74e
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2016-02-06 13:48:03 +0100

    REST: CarController, removing non required import

--- REST/web-services-spring-rest-server/src/main/java/de/spring/webservices/rest/controller/CarController.java
+++ REST/web-services-spring-rest-server/src/main/java/de/spring/webservices/rest/controller/CarController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-import javax.ws.rs.Path;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
