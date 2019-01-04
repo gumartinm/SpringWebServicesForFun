eef9e0ce90421d6fde657251044db815f4c181b7
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2019-01-04 06:33:01 +0100

    Do not use pact-jvm-provider-spring-mvc_2.11

--- PACT/web-services-spring-pact-server/src/test/java/de/example/spring/pact/infrastructure/controller/CarControllerIntegrationTest.java
+++ PACT/web-services-spring-pact-server/src/test/java/de/example/spring/pact/infrastructure/controller/CarControllerIntegrationTest.java
@@ -1,63 +1,55 @@
 package de.example.spring.pact.infrastructure.controller;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import com.reagroup.pact.provider.PactRunner;
-import com.reagroup.pact.provider.ProviderState;
-
-import au.com.dius.pact.provider.junit.loader.PactBroker;
-import de.example.spring.pact.domain.entity.Car;
-import de.example.spring.pact.domain.service.CarServiceImpl;
-
-import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
 import java.util.Collections;
+import java.util.List;
 
-@RunWith(PactRunner.class)
-@PactBroker(host = "localhost", port = "80")
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.RestPactRunner;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.target.MockMvcTarget;
+import de.example.spring.pact.domain.entity.Car;
+import de.example.spring.pact.domain.service.CarServiceImpl;
+
+@RunWith(RestPactRunner.class)
+@Provider("some_provider")
+@PactBroker(host = "${pactbroker.host:localhost}", port = "${pactbroker.port:80}")
 public class CarControllerIntegrationTest {
 
-	@Autowired
 	private CarController carController;
 
 	private CarServiceImpl carServiceImpl;
 
-	private MockMvc mockMvc;
+	@TestTarget
+	private MockMvcTarget mockMvc;
 
 	@Before
 	public void setUpClass() {
 		carServiceImpl = mock(CarServiceImpl.class);
-		mockMvc = MockMvcBuilders.standaloneSetup(new CarController(carServiceImpl)).build();
+		carController = new CarController(carServiceImpl);
+		mockMvc = new MockMvcTarget();
+		mockMvc.setControllers(carController);
 	}
 
-    @ProviderState("some_provider")
+    @State("state")
 	@Test
-	public CarController shouldFindAll() throws Exception {
+	public void shouldFindAll() throws Exception {
 		Car car = new Car.CarBuilder().withBrand("Ford").withEngine("Diesel").build();
 		List<Car> cars = Collections.singletonList(car);
 		given(carServiceImpl.findAll()).willReturn(cars);
 
-		mockMvc
-			.perform(get("/cars/"))
-			.andExpect(jsonPath("$.[0].brand", is("Ford")))
-			.andExpect(jsonPath("$.[0].engine", is("Diesel")))
-			.andExpect(status().isOk());
+		mockMvc.setRunTimes(1);
 
 		verify(carServiceImpl, times(1)).findAll();
-
-		return carController;
 	}
 }
