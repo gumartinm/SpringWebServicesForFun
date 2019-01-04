9ea49e8aa7dc4af2ad06e777016b3119c725f24b
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2019-01-04 15:03:01 +0100

    web-services-spring-pact-server: PAC test

--- PACT/web-services-spring-pact-server/src/test/java/de/example/spring/pact/infrastructure/controller/CarControllerPACIntegrationTest.java
+++ PACT/web-services-spring-pact-server/src/test/java/de/example/spring/pact/infrastructure/controller/CarControllerPACIntegrationTest.java
@@ -1,51 +1,54 @@
 package de.example.spring.pact.infrastructure.controller;
 
-import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.junit.runner.RunWith;
 
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.RestPactRunner;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.target.MockMvcTarget;
 import de.example.spring.pact.domain.entity.Car;
 import de.example.spring.pact.domain.service.CarServiceImpl;
 
-public class CarControllerIntegrationTest {
+@RunWith(RestPactRunner.class)
+@Provider("some_provider")
+@PactBroker(host = "${pactbroker.host:localhost}", port = "${pactbroker.port:80}")
+public class CarControllerPACIntegrationTest {
 
 	private CarController carController;
 
 	private CarServiceImpl carServiceImpl;
 
-	private MockMvc mockMvc;
+	@TestTarget
+	private MockMvcTarget mockMvc;
 
 	@Before
 	public void setUpClass() {
 		carServiceImpl = mock(CarServiceImpl.class);
 		carController = new CarController(carServiceImpl);
-		mockMvc = MockMvcBuilders.standaloneSetup(carController).build();
+		mockMvc = new MockMvcTarget();
+		mockMvc.setControllers(carController);
 	}
 
+    @State("state")
 	@Test
 	public void shouldFindAll() throws Exception {
 		Car car = new Car.CarBuilder().withBrand("Ford").withEngine("Diesel").build();
 		List<Car> cars = Collections.singletonList(car);
 		given(carServiceImpl.findAll()).willReturn(cars);
 
-		mockMvc
-		.perform(get("/cars/"))
-		.andExpect(jsonPath("$.[0].brand", is("Ford")))
-		.andExpect(jsonPath("$.[0].engine", is("Diesel")))
-		.andExpect(status().isOk());
+		mockMvc.setRunTimes(1);
 
 		verify(carServiceImpl, times(1)).findAll();
 	}
