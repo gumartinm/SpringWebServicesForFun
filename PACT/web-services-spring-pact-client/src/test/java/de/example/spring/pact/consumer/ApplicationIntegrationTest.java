1229c03c167c60f53d29da8e65a106a5e7dacd47
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2019-01-04 16:55:09 +0100

    Required dependency spring-cloud-starter-netflix-ribbon

--- PACT/web-services-spring-pact-client/src/test/java/de/example/spring/pact/consumer/ApplicationIntegrationTest.java
+++ PACT/web-services-spring-pact-client/src/test/java/de/example/spring/pact/consumer/ApplicationIntegrationTest.java
@@ -7,17 +7,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.cloud.netflix.ribbon.StaticServerList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.github.tomakehurst.wiremock.common.ClasspathFileSource;
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
-
-import de.example.spring.pact.consumer.Application;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { Application.class },
+@SpringBootTest(classes = { Application.class,
+						    ApplicationIntegrationTest.RibbonTestConfiguration.class },
                 webEnvironment = WebEnvironment.NONE)
 @ActiveProfiles("test")
 public class ApplicationIntegrationTest {
@@ -31,4 +35,13 @@ public class ApplicationIntegrationTest {
 		
 	}
 
+	@Configuration
+	public static class RibbonTestConfiguration {
+
+		@Bean
+	    public ServerList<Server> serverList() {
+	      return new StaticServerList<>(new Server("localhost", wireMockRule.port()));
+	    }
+
+	}
 }
